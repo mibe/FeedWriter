ec32294a3daa97457ad83399f56218bf4fced04d
Michael Bemmerl <mail@mx-server.de>
2011-03-06 03:34:37 +0100

    Don't use a (actually not working) database for retrieving item information.
    The minimal example should have minimal requirements...

--- examples/example_minimum.php
+++ examples/example_minimum.php
@@ -34,24 +34,23 @@
   //Image title and link must match with the 'title' and 'link' channel elements for valid RSS 2.0
   $TestFeed->setImage('Testing the RSS writer class','http://www.ajaxray.com/projects/rss','http://www.rightbrainsolution.com/_resources/img/logo.png');
   
-	//Retriving informations from database addin feeds
-	$db->query($query);
-	$result = $db->result;
-
-	while($row = mysql_fetch_array($result, MYSQL_ASSOC))
-	{
-		//Create an empty FeedItem
-		$newItem = $TestFeed->createNewItem();
-		
-		//Add elements to the feed item    
-		$newItem->setTitle($row['title']);
-		$newItem->setLink($row['link']);
-		$newItem->setDate($row['create_date']);
-		$newItem->setDescription($row['description']);
-		
-		//Now add the feed item
-		$TestFeed->addItem($newItem);
-	}
+  //Let's add some feed items: Create two empty FeedItem instances
+  $itemOne = $TestFeed->createNewItem();
+  $itemTwo = $TestFeed->createNewItem();
+  
+  //Add item details
+  $itemOne->setTitle('The title of the first entry.');
+  $itemOne->setLink('http://www.google.de');
+  $itemOne->setDate(time());
+  $itemOne->setDescription('And here\'s the description of the entry.');
+  $itemTwo->setTitle('Lorem ipsum');
+  $itemTwo->setLink('http://www.example.com');
+  $itemTwo->setDate(1234567890);
+  $itemTwo->setDescription('Lorem ipsum dolor sit amet, consectetur, adipisci velit');
+  
+  //Now add the feed item
+  $TestFeed->addItem($itemOne);
+  $TestFeed->addItem($itemTwo);
   
   //OK. Everything is done. Now genarate the feed.
   $TestFeed->generateFeed();
